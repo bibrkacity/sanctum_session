38626f6e5f08ee01caf332cf678466e36aea0a3e
Yuriy A. Maksymenko <bibrkacity@gmail.com>
2026-03-20 17:40:59 +0200

    Add ServiceProvider.php

---
+++ src/app/Providers/ServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bibrkacity\SanctumSession\app\Providers;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->publishesMigrations([
+            __DIR__.'/../../database/migrations' => database_path('migrations'),
+        ]);
+    }
+}
